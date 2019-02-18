28d86778adf6eec0e04b2873a8f8a4f11278f452
Maximilian Gaethje <maximilian.gaethje@outlook.com>
2019-02-18 14:37:53 +0100

    ex 1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,18 @@
 class QuestionsController < ApplicationController
   def ask
+    # get input from view
   end
 
   def answer
+    @input = params[:question]
+    if @input.include?("?")
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @input == 'Silly question, get dressed and go to work!'
+      @answer = 'Great'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
+# read questions from params and compute and instance variable @answer for the view to display
