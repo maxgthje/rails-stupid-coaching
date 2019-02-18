e5c3da3c3f021adce3d293f23813518b303e5102
Maximilian Gaethje <maximilian.gaethje@outlook.com>
2019-02-18 14:51:11 +0100

    ex1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
     @input = params[:question]
     if @input.include?("?")
       @answer = 'Silly question, get dressed and go to work!'
-    elsif @input == 'Silly question, get dressed and go to work!'
+    elsif @input == 'I am going to work'
       @answer = 'Great'
     else
       @answer = "I don't care, get dressed and go to work!"
